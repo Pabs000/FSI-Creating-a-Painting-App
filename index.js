b1984bbd16f433ecc64f539dd88d702aa204d598
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-02-02 21:24:51 -0800

    adding testing 123

---
+++ index.js
@@ -0,0 +1,19 @@
+<><div class="palette">
+    <div class="color-choice" id="blue">■</div>
+    <div class="color-choice" id="red">■</div>
+    <div class="color-choice" id="yellow">■</div>
+    <div class="color-choice" id="green">■</div>
+
+
+</div><div class="painting">
+        <div class="pixel"></div>
+        <div class="pixel"></div>
+        <div class="pixel"></div>
+        ...</></>
+
+
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(){
+    console.log('testing, testing, 123')
+})
